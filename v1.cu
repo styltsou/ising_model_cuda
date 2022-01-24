45810282588395719405575547bd10fec4940c7a
styltsou <tsoumanis.stylianos@gmail.com>
2022-01-24 21:45:02 +0200

    Added kernel for matrix padding. Optimizations needed

--- v1.cu
+++ v1.cu
@@ -4,23 +4,50 @@
 
 #include "utils.h"
 #include "v1.h"
-
+ 
 /*
   TODO: Might be a good idea to implement a kernel for matrix padding
   instead of performing in on host
 */
 
+__global__ void padd_matrix(int *matrix, int size, int *pad_matrix) {
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  int j = blockIdx.y * blockDim.y + threadIdx.y;
+
+  // Ensur the indexes stay within bounds
+  if (i < size && j < size) {
+    // Copy elements from matrix to padded matrix
+    pad_matrix[(i + 1) * (size + 2) + (j + 1)] = matrix[i * size + j];
+
+    // Add top padding
+    pad_matrix[i + 1] = matrix[(size - 1) * size + i];
+    // Add right padding
+    pad_matrix[(i + 1) * (size + 2) + (i + 1)] = matrix[i * size];
+    // Bottom padding
+    pad_mat[(size + 1) * (size + 2) + (i + 1)] = matrix[i];
+    // Add left padding
+    pad_mat[(i + 1) * (size + 2)] = matrix[i * size + (size - 1)];
+  }
+}
+
 // Define the kernel to calculate a moment per thread
-__global__ void calc_moment(int *pad_in_matrix, int *pad_out_matrix, int size) {
+__global__ void calc_moment(int *pad_in_matrix, int *out_matrix, int msize) {
   int i = blockIdx.x * blockDim.x + threadIdx.x;
   int j = blockIdx.y * blockDim.y + threadIdx.y;
 
   // Computation must not be performed on the border elements
-  if (i < size - 1 && j < size - 1) {
+  if (i < msize - 1 && j < msize - 1) {
     // calcualte moment and update out matrix
-    pad_out_matrix[(i + 1) * size + (j + 1)] =
+    pad_out_matrix[(i + 1) * msize + (j + 1)] =
         calculate_moment(pad_in_matrix, i + 1, j + 1);
   }
+}
+
+void update_ising_model(int *matrix, int *out_matrix, int model_size) {
+  // function that performs the update of the model
+  // pass in matrix, out matrix returns with the updated spins
+  // the function also calls the kernel 
+  // and handles memory copies maybe
 }
 
 void ising_model_v1(int *in_matrix, int *out_matrix, int model_size,
@@ -50,9 +77,17 @@ void ising_model_v1(int *in_matrix, int *out_matrix, int model_size,
   int GRID_SIZE = ceil(model_size / BLOCK_SIZE);
   dim3 grid_dim(GRID_SIZE, GRID_SIZE, 1);
 
-  // THIS WILL BE KIND OF TRICKY (MAYBE theres no need for swap)
-  // Matrix pad may be needed before launcing the kernel
-  // for number of iterations,
+  int k = 0;
+  while (k < num_iterations) {
+    // START UPDATE ISING
+
+    // EMD UPDATE ISING
+
+    // SWAP HERE
+  
+    k++;
+  }
+
   // Check weather in or out matrix contains the result and swap if needed
 
   // Copy data back from the device
