559df713d0cef05b11b2ebc3fe5e90ca73bc031a
styltsou <tsoumanis.stylianos@gmail.com>
2022-01-27 15:21:34 +0200

    Removed vscode settings and fixed some errors in main

--- main.cu
+++ main.cu
@@ -21,16 +21,16 @@ int main(int argc, char **argv) {
   int *out_matrix_v1 = (int *)malloc(model_size * model_size * sizeof(int));
 
   ising_model(in_matrix, out_matrix_v0, model_size, num_iterations);
-  //ising_model_v1(in_matrix, out_matrix_v1, model_size, num_iterations);
+  ising_model_v1(in_matrix, out_matrix_v1, model_size, num_iterations);
 
   printf("Model state after %d iterations\n", num_iterations);
   print_model_state(out_matrix_v0, model_size);
 
-  //if (compare_matrices(out_matrix_v0, out_matrix_v1)) {
-    //printf("\nV1 is correct\n")
-  //} else {
-    //printf("\nV1 has a problem\n")
-  //}
+  if (compare_matrices(out_matrix_v0, out_matrix_v1, model_size)) {
+    printf("\nV1 is correct\n");
+  } else {
+    printf("\nV1 has a problem\n");
+  }
 
   // Clean up
   free(in_matrix);
