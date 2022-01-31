9c10b5a055ad37a728e05ab5717b31c5dff9b8fa
styltsou <tsoumanis.stylianos@gmail.com>
2022-01-31 18:24:49 +0200

    Fixed header file

--- v3.h
+++ v3.h
@@ -8,7 +8,7 @@ __global__ void add_halo_v3(int *in_matrix, int size, int tile_width,
 
 __global__ void update_model_v3(int *pad_in_matrix, int *out_matrix, int size);
 
-int *ising_model_v2(int *in_matrix, int size, int tile_width,
+int *ising_model_v3(int *in_matrix, int size, int tile_width,
                     int num_iterations);
 
 #endif
