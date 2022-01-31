d9bd7ad8bec79f7b408ccb261c224805f50fd30d
styltsou <tsoumanis.stylianos@gmail.com>
2022-01-31 19:20:54 +0200

    Further tests

--- v3.cu
+++ v3.cu
@@ -91,7 +91,7 @@ int *ising_model_v3(int *in_matrix, int size, int tile_width,
   int *pad_in_matrix_d;
   int *out_matrix_d;
 
-  int pad_in_matrix = (int *)malloc(pad_matrix_bytes);
+  int *pad_in_matrix = (int *)malloc(pad_matrix_bytes);
 
   cudaMalloc((void **)&in_matrix_d, matrix_bytes);
   cudaMalloc((void **)&pad_in_matrix_d, pad_matrix_bytes);
@@ -116,7 +116,7 @@ int *ising_model_v3(int *in_matrix, int size, int tile_width,
     cudaMemcpy(pad_in_matrix, pad_in_matrix_d, pad_matrix_bytes,
                cudaMemcpyDeviceToHost);
     printf("Padded matrix\n");
-    print_model_state(pad_in_matrix, model_size + 2);
+    print_model_state(pad_in_matrix, size + 2);
 
     update_model_v3<<<grid_dim, block_dim, shared_mem_bytes>>>(
         pad_in_matrix_d, out_matrix_d, size, tile_width);
